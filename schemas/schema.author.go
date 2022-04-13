6728c235bf8094b99bdab32faf74ec9be25c860c
Andrianto Cahyono Putro <andri.c.putro@gmail.com>
2022-04-13 20:19:05 +0700

    Update schema.author.go

--- schemas/schema.author.go
+++ schemas/schema.author.go
@@ -3,6 +3,7 @@ package schemas
 type Author struct {
 	ID int `json:"id"`
 	Name string `json:"name"`
+	Email string `json:"email"`
 	Username string `json:"username"`
 	Password string `json:"password"`
 	Article *Article `gorm:"foreignkey:AuthorID" json:"article,omitempty"`
